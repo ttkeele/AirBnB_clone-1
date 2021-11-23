5b904b5a8a4a5cc4c0ef649f696d1c962fe56e45
ttkeele <3264@holbertonschool.com>
2021-11-23 22:05:29 +0000

    fixed indent

--- console.py
+++ console.py
@@ -147,7 +147,7 @@ class HBNBCommand(cmd.Cmd):
                 pass
         new_instance.save()
         print(new_instance.id)
-        
+
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
