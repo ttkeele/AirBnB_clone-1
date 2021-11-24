df462c00600a0a75465d650100625e5cc02d377b
Niqualis <NicholasJWallace@protonmail.com>
2021-11-24 11:11:40 -0600

    test

--- models/user.py
+++ models/user.py
@@ -3,7 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
@@ -13,9 +14,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship('Place',
-                          cascade="all, delete, delete-orphan",
-                          backref='user')
-    reviews = relationship('Review',
-                           cascade="all, delete, delete-orphan",
-                           backref='user')
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
