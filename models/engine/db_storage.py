fbdfbda692668fc1f73aa6e20fd8814d2fb1c225
Niqualis <NicholasJWallace@protonmail.com>
2021-11-24 17:50:24 -0600

    test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 """defines a class to manage database storage"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from os import getenv
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from models.user import User
-from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, scoped_session
 import os
+
+
+user = os.getenv('HBNB_MYSQL_USER')
+pwd = os.getenv('HBNB_MYSQL_PWD')
+host = os.getenv('HBNB_MYSQL_HOST')
+db = os.getenv('HBNB_MYSQL_DB')
+env = os.getenv('HBNB_ENV')
 
 
 class DBStorage:
@@ -21,47 +27,50 @@ class DBStorage:
 
     def __init__(self):
         """initializes the storage"""
-        usr = getenv('HBNB_MYSQL_USER')
-        pwd = getenv('HBNB_MYSQL_PWD')
-        host = getenv('HBNB_MYSQL_HOST')
-        db = getenv('HBNB_MYSQL_DB')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            usr, pwd, host, db), pool_pre_ping=True)
-        if getenv('HBNB_ENV') == 'test':
-            Base.metadata.drop_all(self.__engine)
+            user, pwd, host, db), pool_pre_ping=True)
+    if env == "test":
+        Base.MetaData.drop_all()
 
     def all(self, cls=None):
-        """Returns a dictionaary of all objects in DB"""
-        if cls in mapped_classes:
-            return {"{}.{}".format(cls.__name__, item.id): item
-                    for item in self.__session.query(cls)}
+        """Returns a dictionary of all objects in DB"""
+        my_dict = {}
+        if cls in self.__classes:
+            result = DBStorage.__session.query(cls)
+            for row in result:
+            key = "{}.{}".format(row.__class__.__name__, row.id)
+            my_dict[key] = row
         else:
-            objs = {}
-            for c in mapped_classes:
-                objs.update({"{}.{}".format(c.__name__, item.id): item
-                             for item in self.__session.query(c)})
-                return objs
+            for cls in self.__classes:
+                result = DBStorage.__session.query(cls)
+                for row in result:
+                    key = "{}.{}".format(row.__class__.__name__, row.id)
+                    my_dict[key] = row
+        return my_dict
 
 
     def new(self, obj):
         """adds an object to the session"""
-        if type(obj) in mapped_classes:
-            self.__session.add(obj)
+        DBStorage.__session.add(obj)    
 
 
     def save(self):
         """commits the changes in  the current session"""
-        self.__session.commit()
+        DBStorage.__session.commit()
 
 
     def delete(self, obj=None):
         """deletes an object from the session"""
-        if obj in self.all(type(obj).values()):
-            self.__session.delete(obj)
+        DBStorage.__session.delete(obj)
 
 
     def reload(self):
         """reloads from the database"""
         Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(sessionmaker(bind=self.__engine,
-                                                     expire_on_commit=False))
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        DBStorage.__session = scoped_session(session_factory)
+        DBStorage.__session = session()
+
+    def close(self):
+        """public method to call remove method"""
+        DBStorage.__session.close()
