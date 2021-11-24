e8bf5a7e2bb486cc122f188b4d8c38cd4c1074c5
Niqualis <NicholasJWallace@protonmail.com>
2021-11-24 09:47:12 -0600

    test

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-
+import models
 Base = declarative_base()
 
 class BaseModel:
