ba2f1d409204e3c90e82218f5019c83381a951d5
ttkeele <3264@holbertonschool.com>
2021-11-25 03:58:35 +0000

    fixed indent

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -10,4 +12,5 @@ class Amenity(BaseModel, Base):
     """Amenity class"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    place_amenities = relationship("Place", secondary=place_amenity,
+                                   backref='places')
