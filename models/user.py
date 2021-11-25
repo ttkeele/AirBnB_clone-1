682548048fc8570fe8ebe018721bc1803bf4476d
ttkeele <3264@holbertonschool.com>
2021-11-25 04:47:56 +0000

    changed import froms

--- models/user.py
+++ models/user.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+
+
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
+
 
 
 class User(BaseModel, Base):
