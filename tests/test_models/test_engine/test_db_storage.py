438a3743ab4004fad7cfe2a9bedddb79c5f2b3a8
ttkeele <3264@holbertonschool.com>
2021-11-27 18:40:54 +0000

    fixed test cases

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,47 +1,37 @@
 #!/usr/bin/python3
-""" Module for testing DBstorage"""
+""" Module for testing MySQL DBstorage"""
 import unittest
-from models.base_model import BaseModel
+from models import Amenity, City, Place, Review, State, User
 from models import storage
+from models.engine.db_storage import DBStorage
 import os
-from models.state import State
+import MySQLdb
+
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
+                 "using database storage")
 
 class test_dbstorage(unittest.TestCase):
     """ Class to test the DBstorage method """
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                     "Cannot storage if db is active")
-    def setUp(self):
-        """ Set up test environment """
-        del_list = []
-        for key in storage.all().keys():
-            del_list.append(key)
-        for key in del_list:
-            storage._DBStorage__session.delete(storage.all()[key])
-            storage._DBStorage__session.commit()
+    @classmethod
+    def setUpClass(cls):
+        cls.db = MySQLdb.connect(host=os.getenv('HBNB_MYSQL_HOST'),
+                                 user=os.getenv('HBNB_MYSQL_USER'),
+                                 passwd=os.getenv('HBNB_MYSQL_PWD'),
+                                 db=os.getenv('HBNB_MYSQL_DB'),
+                                 port=3306
+                                 )
+        cls.cur = cls.db.cursor()
 
-    def test_obj_list_empty(self):
-        """ __objects is initially empty """
-        self.assertEqual(len(storage.all()), 0)
+    @classmethod
+    def tearDownClass(cls):
+        cls.cur.close()
+        cls.db.close()
 
-    def test_reload_from_nonexistent(self):
-        """ Nothing happens if file does not exist """
-        self.assertEqual(storage.reload(), None)
+    def test_instance(self):
+        """confirms instance"""
+        self.assertIsInstance(storage, DBStorage)
 
-    def test_type_objects(self):
-        """ Confirm __objects is a dict """
-        self.assertEqual(type(storage.all()), dict)
-
-
-    def test_store(self):
-        """ Test if an object is store in the database """
-        new = State(name="Oregon")
-        new.save()
-        _id = new.to_dict()['id']
-        self.assertIn(new.__class__.__name__ + '.' + _id,
-                      storage.all(type(new)).keys())
-
-    def test_storage_var_created(self):
-        """ FileStorage object storage created """
-        from models.engine.db_storage import DBStorage
-        self.assertEqual(type(storage), DBStorage)
+    def test_access(self):
+        self.cur.execute("SELECT * from states")
+        self.assertTrue(self.cur.fetchall() != None)
