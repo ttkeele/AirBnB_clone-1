469c8ccabb2b7de021fe5840beb492bf22d04167
ttkeele <3264@holbertonschool.com>
2021-11-27 16:42:17 +0000

    fixed places and reviews for delete

--- models/user.py
+++ models/user.py
@@ -16,5 +16,9 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="all, delete")
-    reviews = relationship("Review", backref="user", cascade="all, delete")
+    places = relationship("Place",
+                          cascade="all, delete, delte-orphan",
+                          backref='user')
+    reviews = relationship("Review",
+                           cascade="all, delete, delete-orphan",
+                           backref='user')
