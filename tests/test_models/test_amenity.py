36f0d2189c4c6c5b96449e9e60a8f276558477b4
ttkeele <3264@holbertonschool.com>
2021-11-27 17:37:55 +0000

    fixed import, def

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,44 +2,20 @@
 """test for amenity"""
 
 
-import unittest
-import models
 from models.amenity import Amenity
 from tests.test_models.test_base_model import test_basemodel
-from models.amenity import Amenity
-from sqlalchemy.exc import OperationalError
-from os import getenv
-
 
 class test_Amenity(test_basemodel):
-    """defines test for amenity"""
+    """"""
 
     def __init__(self, *args, **kwargs):
-        """ Initialisation of Amenity instance """
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
-        self.amenity = Amenity(name="wifi")
+        self.dict = {'name': 'Test_amenity'}
 
-    def test_creation(self):
-        """Test Amenity class instance creation """
-        amenity = self.value(id="001", name="wifi")
-        self.assertEqual(amenity.name, "wifi")
-        self.assertEqual(amenity.id, "001")
-
-    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "not supported")
-    def test_no_name(self):
-        """ Check for mandatory arguments"""
-        new = self.value()
-        with self.assertRaises(OperationalError):
-            try:
-                new.save()
-            except Exception as error:
-                models.storage._DBStorage__session.rollback()
-                raise error
-
-    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') == 'db', "not supported")
-    def test_amenity_instance(self):
-        """ Check that amenity is an instance of Amenity class"""
-        amenity = self.value()
-        self.assertTrue(isinstance(amenity, Amenity))
+    def test_name2(self):
+        """ """
+        new = self.value(**self.dict)
+        self.assertEqual(type(new.name), str)
