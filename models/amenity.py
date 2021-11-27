964b7f506587607de30ca1ed9d61ea74939f4e10
ttkeele <3264@holbertonschool.com>
2021-11-27 16:14:54 +0000

    redifined model

--- models/amenity.py
+++ models/amenity.py
@@ -12,5 +12,3 @@ class Amenity(BaseModel, Base):
     """Amenity class"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity,
-                                   backref='places')
