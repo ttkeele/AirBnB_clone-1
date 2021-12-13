5c9bc1dce243922a3fde4fe3d5be9d97d8f20058
ttkeele <3264@holbertonschool.com>
2021-12-13 20:13:22 +0000

    fixed spacing

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,11 +2,13 @@
 """Starts a Flask app"""
 
 if __name__ == '__main__':
-from flask import Flask, render_template
+
+    from flask import Flask, render_template
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def hello():
@@ -15,12 +17,14 @@ def hello():
     """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb')
 def hbnb_route():
     """
     route for '/hbnb'
     """
     return "HBNB"
+
 
 @app.route('/c/<text>')
 def c_route(text):
@@ -29,6 +33,7 @@ def c_route(text):
     replaces '_' in <text> with ' '
     """
     return "C {}".format(text.replace('_', ' '))
+
 
 @app.route('/python/', defaults={'text': 'is cool'})
 @app.route('/python/<text>')
@@ -40,6 +45,7 @@ def python_route(text="is cool"):
     """
     return "Python {}".format(text.replace('_', ' '))
 
+
 @app.route('/number/<int:n>')
 def number_route(n):
     """
@@ -48,11 +54,13 @@ def number_route(n):
     """
     return "{} is a number".format(n)
 
- @app.route('/number_template/<int:n>')
- def number_template(n):
-     """
-     displays number using a template
-     """
-     return render_template('5-number.html', n=n)
 
- app.run(host='0.0.0.0', port='5000')
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """
+    displays number using a template
+    """
+    return render_template('5-number.html', n=n)
+
+
+app.run(host='0.0.0.0', port='5000')
