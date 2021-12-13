3efc69ddd817365ead297ea87defb277df13c8c7
ttkeele <3264@holbertonschool.com>
2021-12-13 16:01:37 +0000

    trying this

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -40,12 +40,12 @@ if __name__ == '__main__':
         """
         return "Python {}".format(text.replace('_', ' '))
 
-    @app.route('/number/<int:n>')
+    @app.route('/number/<n>')
         def number_route(n):
         """
         displays '<n> is a number'
         only accepts integers
         """
-        return "{:d} is a number".format(n)
+        return "{} is a number".format(n)
 
     app.run(host='0.0.0.0', port='5000')
