fb9951698bfafa1a15518fcf9d61cd7182447411
ttkeele <3264@holbertonschool.com>
2021-12-13 16:12:57 +0000

    fixed indent

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -45,8 +45,8 @@ def number_route(n):
     displays '<n> is a number'
     only accepts integers
     """
-    return "{:d} is a number".format(n)
+    return "{} is a number".format(n)
 
 if __name__ == '__main__':
-    
-app.run(host='0.0.0.0', port='5000')
+
+    app.run(host='0.0.0.0', port='5000')
