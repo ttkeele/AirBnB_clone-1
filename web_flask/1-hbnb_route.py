1f8913873b80fb9910d573bdd6a8b2ac727cecee
ttkeele <3264@holbertonschool.com>
2021-12-13 13:38:50 +0000

    sets route for flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Starts a Flask app"""
+
+
+if __name__ == '__main__':
+    from flask import Flask
+
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
+
+    @app.route('/')
+    def hello():
+        """
+        sets the route for '/'
+        """
+        return "Hello HBNB!"
+
+    @app.route('/hbnb')
+    def hbnb_route():
+        """
+        route for '/hbnb'
+        """
+        return "HBNB"
+
+    app.run(host='0.0.0.0', port='5000')
