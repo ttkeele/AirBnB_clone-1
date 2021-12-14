a638b75dff60bad7458bac3022ac8537188c17c2
ttkeele <3264@holbertonschool.com>
2021-12-14 16:20:45 +0000

    moved main function

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Starts a Flask app"""
 
-if __name__ == '__main__':
-
-    from flask import Flask, render_template
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -62,5 +60,5 @@ def number_template(n):
     """
     return render_template('5-number.html', n=n)
 
-
-app.run(host='0.0.0.0', port='5000')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
