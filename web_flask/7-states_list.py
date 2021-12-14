90499aa487299e98c9e38a504582c01e5f49a91b
ttkeele <3264@holbertonschool.com>
2021-12-14 16:41:27 +0000

    defined states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,24 +2,26 @@
 """Starts a Flask app"""
 
 
-if __name__ == '__main__':
-    from flask import Flask, render_template
-    from models import storage
-    from models.state import State
+from flask import Flask, render_template
+from models import storage
+from models.state import State
 
-     app = Flask(__name__)
-     app.url_map.strict_slashes = False
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-     @app.route('/states_list')
-     def states_list():
-         """
-         sets the route for '/states_list'
-         """
-         return render_template('7-states_list.html', states=storage.all(State))
+@app.route('/states_list')
+def states_list():
+    """
+    sets the route for '/states_list'
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=states)
 
-     @app.teardown_appcontext
-     def teardown(context):
-         """resets storage after each request"""
-         storage.close()
+@app.teardown_appcontext
+def teardown(context):
+    """resets storage after each request"""
+    storage.close()
 
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
