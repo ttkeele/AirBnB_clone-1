a3f3c6b7fc6617e26b0c5980994e8977c30cdca7
ttkeele <3264@holbertonschool.com>
2021-12-15 16:10:10 +0000

    fixed spacing

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 """Starts a Flask app"""
 
-if __name__ == '__main__':
-    from flask import Flask, render_template
-    from models import storage
-    from models.state import State
 
-    app = Flask(__name__)
-    app.url_map.strict_slashes = False
+from flask import Flask, render_template
+from models import storage
+from models.state import State
 
-    @app.route('/cities_by_states')
-    def cities_by_states():
-        """
-        sets route for '/cities_by_states'
-        """
-        return render_template('8-cities_by_states.html',
-                               states=storage.all(State))
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-    @app.teardown_appcontext
-    def teardown(context):
-        """
-        resets storage after each request
-        """
-        storage.close()
 
+@app.route('/cities_by_states')
+def cities_by_states():
+    """
+    sets route for '/cities_by_states'
+    """
+    return render_template('8-cities_by_states.html',
+                           states=storage.all(State))
+
+
+@app.teardown_appcontext
+def teardown(context):
+    """
+    resets storage after each request
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
